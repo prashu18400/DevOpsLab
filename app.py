7fdefa9421b4bb3b793494085168cbeccbd782c0
prashanth <prashanth18400@gmail.com>
2023-02-19 12:04:36 +0530

    Added changes during live demo

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h3>Hi Guys, hope you learnt alot from the session</h3> <br/> <p>For more such sessions, please notify a week prior and I expect the efforts from your side as well</p>"
+    return "<h1>Session,19th feb</h1> <br/><br/><h3>Hi Guys, hope you learnt alot from the session</h3> <br/> <p>For more such sessions, please notify a week prior and I expect the efforts from your side as well</p>"
 
